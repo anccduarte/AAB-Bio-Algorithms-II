7202bff5a4d39b09275519aacc0f066043d798dd
anccduarte <68702529+anccduarte@users.noreply.github.com>
2022-09-25 16:34:47 +0100

    Add files via upload

---
+++ code/DeBruijn.py
@@ -0,0 +1,208 @@
+# -*- coding: utf-8 -*-
+
+from typing import Union
+
+"""
+Class: DeBruijn
+"""
+
+class DeBruijn:
+
+	"""
+	Implementa o algoritmo de DeBruijn para a reconstrução de sequências de DNA.
+	"""
+
+	def __init__(self, kmers: list) -> None:
+		"""
+		Inicializa uma instância da classe DeBruijn, guardando os k-mers introduzidos pelo utilizador, o grafo de
+		DeBruijn construído a partir dos mesmos, e o número de nodos "ignorados" aquando da construção do grafo.
+		Parameters
+		----------
+		:param kmers: Os k-mers introduzidos pelo utilizador
+		"""
+
+		if type(kmers) is not list and type(kmers) is not tuple:
+			raise TypeError("O parâmetro 'kmers' deve ser do tipo 'list' ou 'tuple'.")
+
+		for kmer in kmers:
+			if type(kmer) is not str:
+				raise TypeError("Cada um dos k-mers deve ser do tipo 'str'.")
+
+		self.kmers = sorted([kmer.upper() for kmer in kmers])
+		self.de_bruijn = self.get_de_bruijn()
+		self.ignored_nodes = self.get_ignored_nodes()
+
+	def __str__(self) -> str:
+		"""
+		Imprime os k-mers introduzidos pelo utilizador ordenados lexicograficamente.
+		"""
+		return f"Fragmentos: {', '.join(self.kmers)}"
+
+
+
+	def get_nodes(self) -> list:
+		"""
+		A partir dos k-mers, retorna uma lista de nodos que irão compor o grafo. 
+		"""
+		len_kmers = len(self.kmers[0])
+		return [(k[:len_kmers-1], k[1:]) for k in self.kmers]
+
+	def get_de_bruijn(self) -> dict:
+		"""
+		Retorna o grafo que auxilia na execução do algoritmo de DeBruijn. Consiste num dicionário de dicionários 
+		em que os values dos dicionários interiores correspondem ao número de arcos existentes entre os nodos 
+		que o compõem.
+		de_bruijn = {"aa": {"ab": 1, "ac": 2},
+					 "bb": {"ba": 3, "bc": 2},
+					 "cc": {"ca": 2, "cb": 1}}
+		"""
+		nodes = self.get_nodes()
+		de_bruijn = {}
+		for tup in nodes:
+			fr, to = tup
+			if fr not in de_bruijn: de_bruijn[fr] = {to: 1} 
+			else:
+				if to in de_bruijn[fr]: de_bruijn[fr][to] += 1
+				else: de_bruijn[fr][to] = 1
+		return de_bruijn
+
+	def get_ignored_nodes(self) -> int:
+		"""
+		Retorna o número de nodos no grafo de DeBruijn que são destino, mas não são origem.
+		"""
+		nodes = [node for tup in self.get_nodes() for node in tup]
+		ignored_nodes = 0
+		for node in nodes:
+			if node not in self.de_bruijn: ignored_nodes += 1
+		return ignored_nodes
+
+
+
+	def get_out_degree(self, node: str) -> int:
+		"""
+		Retorna o grau de saída do nodo que toma como parâmetro.
+		Parameters
+		----------
+		:param node: O nodo cujo grau de saída se pretende determinar
+		"""
+		out_deg = 0
+		for k in self.de_bruijn[node]:
+			out_deg += self.de_bruijn[node][k]
+		return out_deg
+
+	def get_in_degree(self, node: str) -> int:
+		"""
+		Retorna o grau de entrada do nodo que toma como parâmetro.
+		Parameters
+		----------
+		:param node: O nodo cujo grau de entrada se pretende determinar
+		"""
+		in_deg = 0
+		for k in self.de_bruijn:
+			if node in self.de_bruijn[k]: in_deg += self.de_bruijn[k][node]
+		return in_deg
+
+	def validate_eulerian(self) -> bool:
+		"""
+		Verifica se um grafo é ou não Euleriano.
+		"""
+		begin = end = 0 
+		res = True
+		for node in self.de_bruijn:
+			out_deg, in_deg = self.get_out_degree(node), self.get_in_degree(node)
+			# verificar condição |out_degree - in_degree| > 1
+			if abs(out_deg - in_deg) > 1:
+				res = False
+				break
+			# verificar número de nodos com out_deg > in_deg e com in_deg > out_deg
+			if out_deg > in_deg: begin += 1
+			elif out_deg < in_deg: end += 1
+		# verificar condição "nº de nodos com (out_deg-in_deg==1) == nº de nodos com (out_deg-in_deg==-1)"
+		# "+ self.ignored_nodes" porque todos os nodos ignorados têm mais um grau de entrada que de saída
+		if begin != (end + self.ignored_nodes): res = False
+		return res
+
+	def get_origins(self) -> Union[list, bool]:
+		"""
+		Caso o grafo seja Euleriano, retorna uma lista com as origens dos contigs. Caso contrário, retorna False.
+		"""
+		valid = self.validate_eulerian()
+		if valid:
+			out = []
+			for node in self.de_bruijn:
+				out_deg, in_deg = self.get_out_degree(node), self.get_in_degree(node)
+				if out_deg - in_deg == 1: out += [node]
+			if not out: out += [list(self.de_bruijn.keys())[0]] # caso todos os nodos sejam balanceados
+		else: out = valid
+		return out
+
+
+
+	def get_loop(self, curr: str) -> list:
+		"""
+		Retorna uma lista de nodos que formam uma laçada no grafo.
+		Parameters
+		----------
+		:param curr: O nodo de origem da laçada
+		"""
+		nodes = []
+		while curr in self.dbr_copy:
+			nxt = list(self.dbr_copy[curr].keys())[0]
+			nodes += [nxt]
+			self.dbr_copy[curr][nxt] -= 1
+			if self.dbr_copy[curr][nxt] == 0: del self.dbr_copy[curr][nxt]
+			if not self.dbr_copy[curr]: del self.dbr_copy[curr]
+			curr = nxt
+		return nodes
+
+	def flat_list(self, node_list: list) -> list:
+		"""
+		Tranforma a lista que toma como parâmetro numa lista unidimensional.
+		Parameters
+		----------
+		:param node_list: A lista a ser transformada
+		"""
+		out = []
+		for item in node_list:
+			if type(item) is list: out += item
+			else: out += [item]
+		return out
+
+	def rebuild_seqs(self) -> Union[list, str]:
+		"""
+		Caso o grafo seja Euleriano, retorna uma lista contendo todos os contigs reconstruídos. Caso contrário, 
+		retorna uma mensagem de erro.  
+		"""
+		self.dbr_copy = {k1: {k2: v for k2, v in self.de_bruijn[k1].items()} for k1 in self.de_bruijn} # cópia
+		origins = self.get_origins() # origins == False caso o grafo não seja Euleriano
+		if origins:
+			contigs = []
+			# enquanto houver origens
+			while origins:
+				# o nodo de origem é atualizado sempre que se inicia o processo de reconstrução de um contig
+				nodes = [origins.pop(0)]
+				# enquanto houver arcos no grafo
+				while self.dbr_copy:
+					# procurar nodo na lista de nodos que seja uma chave do grafo (caso não exista, tem-se um 
+					# contig -> break ***)
+					found = False
+					for i, node in enumerate(nodes):
+						if node in list(self.dbr_copy.keys()):
+							curr = node
+							found = True
+							break
+					if not found: break # ***
+					# construção de uma laçada e incorporação na lista de nodos que irá formar o contig
+					loop = self.get_loop(curr)
+					nodes.insert(i+1, loop)
+					nodes = self.flat_list(nodes)
+				# reconstrução do contig (todos os caracteres do primeiro nodo + último caractere dos nodos seguintes)
+				contig = nodes[0]
+				for node in nodes[1:]:
+					contig += node[-1]
+				# adicionar o contig reconstruído à lista de contigs
+				contigs += [contig]
+			out = contigs
+		else:
+			out = "ERRO: O grafo não é Euleriano!"
+		return out
---
+++ code/Evolutionary.py
@@ -0,0 +1,224 @@
+# -*- coding: utf-8 -*-
+
+import random
+
+"""
+Class: Entity
+"""
+
+class Entity:
+
+	"""
+	Gera um indivíduo.
+	"""
+
+	def __init__(self, size: int, genes: list, lb: int, ub: int) -> None:
+		"""
+		Inicializa uma instância da classe Entity.
+		Parameters
+		----------
+		:param size: A dimensão do indivíduo
+		:param genes: Os "genes" do indivíduo
+		:param lb: O limite inferior dos números reais que representam os "genes" do indivíduo
+		:param ub: O limite superior dos números reais que representam os "genes" do indivíduo
+		"""
+		self.size = size
+		self.genes = genes
+		self.lb, self.ub = lb, ub
+		self.fitness = 0
+		self.positions = []
+		if not self.genes:
+			self.genes = [random.uniform(lb, ub) for i in range(size*4)]
+
+
+"""
+Class: Evolutionary
+"""
+
+class Evolutionary:
+
+	"""
+	Implementa um algoritmo evolucionário para a procura de motifs num conjunto de sequências de DNA.
+	"""
+
+	def __init__(self, num_indivs: int, size_indiv: int, num_iter: int, alignment: str) -> None:
+		"""
+		Inicializa uma instância da classe Evolutionary.
+		Parameters
+		----------
+		:param num_indivs: Número de indivíduos na população
+		:param size_indiv: Dimensão de cada um dos indivíduos da população
+		:param num_iter: Número de iterações a efetuar pelo algoritmo evolucionário
+		:param alignment: O alinhamento de sequências no qual se pretende procurar motifs
+		"""
+
+		if type(num_indivs) is not int:
+			raise TypeError("O parâmetro 'num_indivs' deve ser do tipo 'int'.")
+		
+		if type(size_indiv) is not int:
+			raise TypeError("O parâmetro 'size_indiv' deve ser do tipo 'int'.")
+		
+		if type(num_iter) is not int:
+			raise TypeError("O parâmetro 'num_iter' deve ser do tipo 'int'.")
+		
+		if type(alignment) is not str:
+			raise TypeError("O parâmetro 'alignment' deve ser do tipo 'str'.")
+
+		self.num_indivs = num_indivs
+		self.size_indiv = size_indiv
+		self.population = [Entity(size_indiv, [], 0, 1) for i in range(num_indivs)]
+		self.num_iter = num_iter
+		self.alignment = [line.strip().upper() for line in open(alignment).readlines() if line.strip()]
+
+	def __str__(self) -> str:
+		"""
+		Retorna os motifs representados pelos genes de cada indivíduo da população.
+		"""
+		out = []
+		for i, indiv in enumerate(self.population):
+			out += [f"{i+1}: {self.__get_best_seq(self.__get_pwm(indiv))}"]
+		return ", ".join(out)
+
+
+
+	def __get_pwm(self, indiv: list) -> list:
+		"""
+		Retorna um perfil probablístico PWM construído a partir dos "genes" de um indivíduo.
+		Parameters
+		----------
+		:param indiv: O indivíduo para o qual se pretende construír o PWM
+		"""
+		genes = indiv.genes
+		# colocar "genes" no pwm
+		pwm = []
+		for i in range(len(genes)):
+			if i % 4 == 0: 
+				pwm += [{n: genes[i+j] for j,n in enumerate("ATCG")}]
+		# normalizar as colunas do pwm
+		for col in pwm:
+			total = sum(col.values())
+			for k in col:
+				col[k] /= total
+		return pwm
+
+	def __get_best_seq(self, pwm: list) -> str:
+		"""
+		Retorna a sequência com maior probabilidade de ter sido gerada pelo PWM que toma como parâmetro.
+		Parameters
+		----------
+		:param pwm: O perfil probablístico PWM 
+		"""
+		best_seq = ""
+		for col in pwm:
+			sorted_values = sorted(col.items(), key = lambda x: x[1])
+			best_seq += sorted_values[-1][0]
+		return best_seq
+
+	def __get_indiv_attr(self, seq: str) -> tuple:
+		"""
+		Retorna os valores de fitness de cada indivíduo e as posições no alinhamento de sequências que melhor se 
+		adequam ao mesmo.
+		Parameters
+		----------
+		:param seq: A sequência de DNA (motif) associada cada indivíduo.
+		"""
+		max_score, positions = 0, []
+		for sequence in self.alignment:
+			score, pos = 0, 0
+			for i in range(len(sequence)-len(seq)+1):
+				scr = 0
+				for j in range(len(seq)):
+					if sequence[i+j] == seq[j]: scr += 2
+					else: scr -= 1
+				if scr > score:
+					score, pos = scr, i
+			max_score += score
+			positions += [pos]
+		return max_score, positions
+
+	def __evaluate(self) -> None:
+		"""
+		Avalia todos os indivíduos da população num determinado momento, atribuindo-lhes um valor de fitness, e
+		uma lista de posições no alinhamento de sequências associadas ao mesmo.
+		"""
+		for indiv in self.population:
+			pwm = self.__get_pwm(indiv)
+			best_seq = self.__get_best_seq(pwm)
+			indiv.fitness, indiv.positions = self.__get_indiv_attr(best_seq)
+
+
+	def __get_best_indivs(self) -> list:
+		"""
+		Retorna os indivíduos melhor adaptados na população (50%).
+		"""
+		indivs = self.population
+		sorted_indivs = sorted(indivs, key = lambda x: x.fitness)
+		return sorted_indivs[self.num_indivs//2:]
+
+	def __mutation(self, indivs: list) -> None:
+		"""
+		Provoca uma mutação num dos "genes" de cada indivíduo presente na lista que toma como parâmetro.
+		Parameters
+		----------
+		:param indiv: Os indivíduos da população que sofrem mutação
+		"""
+		for indiv in indivs:
+			ind = random.randint(0, self.size_indiv*4-1)
+			new = random.uniform(indiv.lb, indiv.ub)
+			indiv.genes[ind] = new
+
+	def __get_progeny(self, indivs: list) -> list:
+		"""
+		Retorna a descendência de um dado conjunto de indivíduos.
+		Parameters
+		----------
+		:param indivs: Os indivíduos cuja descendência se pretende determinar
+		"""
+		progeny = [Entity(indiv.size, indiv.genes[:], indiv.lb, indiv.ub) for indiv in indivs]
+		idx = random.randint(self.size_indiv, self.size_indiv*3) # gets crossover site
+		for i in range(0, len(progeny)-1, 2):
+			temp = progeny[i].genes[:idx]
+			progeny[i].genes[:idx] = progeny[i+1].genes[:idx]
+			progeny[i+1].genes[:idx] = temp 
+		self.__mutation(progeny)
+		return progeny
+
+	def __new_population(self) -> None:
+		"""
+		Constrói uma nova população tendo por base a população anterior, sendo constituída pelos indivíduos melhor
+		adaptados na população anterior (metade da população) e pela sua descendência.
+		"""
+		best_indivs = self.__get_best_indivs()
+		progeny = self.__get_progeny(best_indivs)
+		self.population = best_indivs + progeny
+
+
+	def __is_max(self) -> bool:
+		"""
+		Caso algum indivíduo da população tenha obtido o score máximo, retorna True. Caso contrário, retorna False.
+		"""
+		res = False
+		for indiv in self.population:
+			if indiv.fitness == 2 * self.size_indiv * len(self.alignment):
+				res = True
+				break
+		return res
+
+	def __run_ea(self) -> None:
+		"""
+		Corre o algoritmo evolucionário.
+		"""
+		for i in range(self.num_iter):
+			self.__evaluate()
+			if self.__is_max(): break
+			self.__new_population()
+
+	def get_motifs(self) -> list:
+		"""
+		Retorna uma lista de motifs presentes no alinhamento de sequências.
+		"""
+		self.__run_ea()
+		sorted_indivs = sorted(self.population, key = lambda x: x.fitness)
+		positions = sorted_indivs[-1].positions
+		motifs = [self.alignment[i][pos:pos+self.size_indiv] for i, pos in enumerate(positions)]
+		return motifs
